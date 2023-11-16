f4f47c56756be52983b035793009add6bd3e6914
HMAsp <hanand55935@stud.noroff.no>
2023-11-16 23:40:00 +0100

    Update vite.config.json with outdir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import path from "path";
 
 export default {
   root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // Specify the output directory here
+  },
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
