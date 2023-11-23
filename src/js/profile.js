f2ae0624794921a8cc347a8d61cc33dac824b5e2
HMAsp <hanand55935@stud.noroff.no>
2023-11-23 19:07:54 +0100

    Adds edit user + new listing form to profile html

--- src/js/profile.js
+++ src/js/profile.js
@@ -4,3 +4,29 @@ import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 import Alert from "bootstrap/js/dist/alert";
 import { Tooltip, Toast, Popover } from "bootstrap";
+
+function containerHandler() {
+  const containerAvatar = document.getElementById("editContainer");
+  const containerListing = document.getElementById("listingContainer");
+  const avatarBtn = document.getElementById("editProfileBtn");
+  const listingBtn = document.getElementById("newListingBtn");
+
+  avatarBtn.addEventListener("click", () => {
+    if (!containerListing.classList.contains("collapse")) {
+      containerListing.classList.add("collapse");
+      containerAvatar.classList.toggle("collapse");
+    } else {
+      containerAvatar.classList.toggle("collapse");
+    }
+  });
+
+  listingBtn.addEventListener("click", () => {
+    if (!containerAvatar.classList.contains("collapse")) {
+      containerAvatar.classList.add("collapse");
+      containerListing.classList.toggle("collapse");
+    } else {
+      containerListing.classList.toggle("collapse");
+    }
+  });
+}
+containerHandler();
