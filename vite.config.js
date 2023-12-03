02e30149b2736bfc75f0440d1bf9283190f099b9
HMAsp <hanand55935@stud.noroff.no>
2023-12-03 13:04:40 +0100

    Updates vite.config for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,23 @@ import path from "path";
 export default {
   build: {
     outDir: path.resolve(__dirname, "./dist"), // Specify the output directory here
-  },
-  resolve: {
-    alias: {
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    rollupOptions: {
+      input: {
+        main: "src/main.js",
+        // Add paths to your HTML files here
+        auction: "src/HTML/auction/index.html",
+        profile: "src/HTML/profile/index.html",
+        // Add more paths if needed for other HTML files
+      },
     },
-  },
-  server: {
-    port: 8080,
-    hot: true,
+    resolve: {
+      alias: {
+        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      },
+    },
+    server: {
+      port: 8080,
+      hot: true,
+    },
   },
 };
