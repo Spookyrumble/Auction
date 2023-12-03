2c1b76889af87510c67135d16691799dc320578a
HMAsp <hanand55935@stud.noroff.no>
2023-12-03 13:07:28 +0100

    Reset vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,14 @@ import path from "path";
 export default {
   build: {
     outDir: path.resolve(__dirname, "./dist"), // Specify the output directory here
-    rollupOptions: {
-      input: {
-        main: "src/main.js",
-        // Add paths to your HTML files here
-        auction: "src/HTML/auction/index.html",
-        profile: "src/HTML/profile/index.html",
-        // Add more paths if needed for other HTML files
-      },
+  },
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
-    resolve: {
-      alias: {
-        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-      },
-    },
-    server: {
-      port: 8080,
-      hot: true,
-    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
   },
 };
