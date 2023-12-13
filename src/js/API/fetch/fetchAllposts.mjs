04e63aa4bf7f268c05d1e4342b55e000d945222c
HMAsp <hanand55935@stud.noroff.no>
2023-12-13 08:09:18 +0100

    Sets fetch limit to 500

--- src/js/API/fetch/fetchAllposts.mjs
+++ src/js/API/fetch/fetchAllposts.mjs
@@ -7,67 +7,48 @@ export async function fetchAllPosts() {
   const limit = 100;
   let offset = 0;
 
-  if (!token) {
-    try {
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        const response = await fetch(
-          `${authListingsURL}&limit=${limit}&offset=${offset}`
-        );
+  const maxPosts = 500;
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
+  try {
+    const headers = token
+      ? {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         }
+      : {};
 
-        const posts = await response.json();
+    const baseUrl = token
+      ? `${authListingsURL}&sort=created&_listings=true`
+      : authListingsURL;
 
-        if (posts.length === 0 || posts.length < limit) {
-          allPostsArray = [...allPostsArray, ...posts];
-          break;
-        }
+    while (allPostsArray.length < maxPosts) {
+      const response = await fetch(
+        `${baseUrl}&limit=${limit}&offset=${offset}`,
+        { headers }
+      );
 
-        allPostsArray = [...allPostsArray, ...posts];
-        offset += limit;
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
       }
 
-      return allPostsArray;
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      return [];
-    }
-  } else {
-    try {
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        const response = await fetch(
-          `${authListingsURL}&limit=${limit}&offset=${offset}&sort=created&_listings=true`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-              authorization: `Bearer ${token}`,
-            },
-          }
-        );
+      const posts = await response.json();
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-
-        const posts = await response.json();
-
-        if (posts.length === 0 || posts.length < limit) {
-          allPostsArray = [...allPostsArray, ...posts];
-          break;
-        }
-
+      if (posts.length === 0 || posts.length < limit) {
         allPostsArray = [...allPostsArray, ...posts];
-        offset += limit;
+        break;
       }
 
-      return allPostsArray;
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      return [];
+      allPostsArray = [...allPostsArray, ...posts];
+      if (allPostsArray.length > maxPosts) {
+        allPostsArray = allPostsArray.slice(0, maxPosts); // Cut off any excess posts
+        break;
+      }
+      offset += limit;
     }
+
+    return allPostsArray;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
   }
 }
